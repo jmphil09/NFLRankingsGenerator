6a7a5b8b347679be5f431bd12b7777392730f6dd
James Phillips <jmphil09@gmail.com>
2014-09-09 10:24:57 -0700

    rankings seem to work fine now

--- src/fileWriter/package.scala
+++ src/fileWriter/package.scala
@@ -2,8 +2,8 @@ import java.io._
 
 package object fileWriter {
 	def fileWriter(filename: String, rankStr: String) = {
-      //val writer = new PrintWriter(new File("C:/Users/James/Desktop/git-workspace/NFLRankingsGenerator/data/" + filename + ".txt" )) //FOR LAPTOP
-      val writer = new PrintWriter(new File("C:/Users/fligh_000/Desktop/git-workspace/NFLRankingsGenerator/data/" + filename + ".txt")) //FOR PC
+      val writer = new PrintWriter(new File("C:/Users/James/Desktop/git-workspace/NFLRankingsGenerator/data/" + filename + ".txt" )) //FOR LAPTOP
+      //val writer = new PrintWriter(new File("C:/Users/fligh_000/Desktop/git-workspace/NFLRankingsGenerator/data/" + filename + ".txt")) //FOR PC
       writer.write(rankStr)
       writer.close()
     }
--- src/mainfile.sc
+++ src/mainfile.sc
@@ -6,5 +6,5 @@ object mainfile {
   println("Welcome to the Scala worksheet")       //> Welcome to the Scala worksheet
   //getRecordMap("2014")
   
-  //generateRankings("2014", "2")
+  //generateRankings("2014", "test")
 }
